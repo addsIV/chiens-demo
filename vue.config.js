73e576816fd15353dc63c395b3e92b5f36784e5c
adds <kl13245768@gmail.com>
2024-11-19 22:25:53 +0800

    change public path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/chiens-demo/' // 將 <repository-name> 替換為你的存儲庫名稱
+      : '/'
+};
